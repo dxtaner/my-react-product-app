0b7a58fef042aaafd74ec2b17b59bb4b0baeef42
taner <tanerozer16@gmail.com>
2024-05-03 14:57:36 +0300

    ProductTableHeader.tsx added

---
+++ src/components/ProductsTable/ProductTableHeader.tsx
@@ -0,0 +1,19 @@
+const ProductTableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Discount Percentage</th>
+        <th>Rating</th>
+        <th>Stock</th>
+        <th>Brand</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default ProductTableHeader;
